a2e9e2a23433e90a16fbeeb4cdfedcec51159721
Jess Walters <jessw@iastate.edu>
2017-10-18 03:58:26 -0500

    now, 50% of the time, works everytime

--- project1.c
+++ project1.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #define MAX_LINE 80 /* The maximum length command */
+
+void parse(char *cmd, char** args) {
+	while(*cmd != '\0') {
+		while(*cmd == ' ') {
+			*cmd++;
+		}
+		*args++ = cmd;
+		while(*cmd != '\0' && *cmd != ' ') {
+			cmd++;
+		}
+	}
+	*args++ = '\0';
+}
+
+void runCommand(char **args) {
+	int status;
+	pid_t pid = fork();
+
+	if (pid == 0) {
+		execvp(*args, args);
+	}
+	else {
+		while(wait(&status) != pid);
+	}
+}
 
 int main(void) {
 	char *args[MAX_LINE/2 + 1]; /* command line arguments */
 	int should_run = 1; /* flag to determine when to exit program */
 	int status;
+	char *cmd;
 	while (should_run) {
 		printf("osh>");
 		fflush(stdout);
-		scanf("%s", cmd);
-		printf("\n %s", &cmd);
+		scanf("%s", cmd);	
 
-		int pid = fork();
-
-		if (pid == 0) {
-			execvp(cmd, args);
-			exit(1);
+		parse(cmd, args);
+		if (strcmp(args[0], "exit") == 0){
+			exit(0);
 		}
-		else {
-			waitpid(pid, &status, 0);
-		}
+		runCommand(args);
 		/**
 		 * After reading user input, the steps are:
 		 * (1) fork a child process using fork()

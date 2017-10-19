951d3e6c59b03e37f8d30c735a67ed7904e6ff28
JessWalters <jessw@iastate.edu>
2017-10-18 23:41:23 -0500

    Ayy its all good, with 1 bug lol

--- project1.c
+++ project1.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 #define MAX_LINE 80 /* The maximum length command */
 
@@ -31,15 +32,72 @@ int isAsync(char *args[]) {
 	return 0;
 }
 
+
 int main(void) {
 	char *args[MAX_LINE/2 + 1]; /* command line arguments */
+    char *history[10];
 	int should_run = 1; /* flag to determine when to exit program */
-	int status;
-	char *cmd = malloc(sizeof(char) * MAX_LINE);
+	char *cmd = (char *) malloc(sizeof(char) * MAX_LINE);
+    char *hist_tmp = (char *) malloc(sizeof(char) * MAX_LINE);
+    char *tmp2 = (char *) malloc(sizeof(char) * MAX_LINE);
+
+    for (int k = 0;  k < 10;  ++k)
+        history [k] = malloc (sizeof(char) * MAX_LINE);
+
 	while (should_run) {
 		printf("osh>");
 		fflush(stdout);
 		fgets(cmd, MAX_LINE, stdin);
+
+        if (strcmp(cmd, "exit\n") == 0){
+            should_run = 0;
+            continue;
+        }
+        if (strcmp(cmd, "history\n") == 0) {
+            // Count number of elements, this is only done for purpose of following assignment formatting
+            // Count is useless otherwise
+            int count = 0;
+            for (int c = 0; c < 10; c++) {
+                if (strcmp(history[c], "") != 0)
+                    count++;
+            }
+            for (int b = 0; b < 10; b++) {
+
+                if (strcmp(history[b], "") != 0) {
+                    printf("%d %s", count, history[b]);
+                    fflush(stdout);
+                    count--;
+                }
+            }
+            continue;
+        }
+        if (strcmp(cmd, "!!\n") == 0) {
+            strcpy(cmd, history[0]);
+        }
+        if (cmd[0] == '!') {
+            if (isdigit(cmd[1])){
+                // Converts char to int
+                int command_num = cmd[1] - '0';
+                strcpy(cmd, history[command_num]);
+            }
+
+        }
+
+        //Save to history
+        for(int j = 0; j < 10; j++) {
+            if (j == 0) {
+                strcpy(hist_tmp, history[j]);
+                strcpy(history[j], cmd);
+                continue;
+            }
+            if (j == 9) {
+                strcpy(history[j], hist_tmp);
+                break;
+            }
+            strcpy(tmp2, hist_tmp);
+            strcpy(hist_tmp, history[j]);
+            strcpy(history[j], tmp2);
+        }
 
 		char *tmp = strtok(cmd, " ");
 		int i = 0;
@@ -51,17 +109,8 @@ int main(void) {
 		for (i = i; i< (MAX_LINE/2 + 1); i++)
 			args[i] = NULL;
 
-		if (strcmp(args[0], "exit") == 0){
-			should_run = 0;
-		}
 		int async = isAsync(args);
 		runCommand(args, async);
-		/**
-		 * After reading user input, the steps are:
-		 * (1) fork a child process using fork()
-		 * (2) the child process will invoke execvp()
-		 * (3) if command included &, parent will invoke wait()
-		 */
 	}
 	return 0;
 }
